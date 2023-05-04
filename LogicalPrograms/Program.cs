dd694675caa7e2811d9cde12599cb6731912697a
Rak65 <rakeshp6505@gmail.com>
2023-05-04 19:30:32 +0530

    Print the Elapsed Time

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace LogicalPrograms
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            ReverseNumber.ReverseNo();     //call the ReverseNo method
-            Console .ReadKey();
+            Stopwatch stopWatch = new Stopwatch();
+            Console.WriteLine("Enter 1 to start the StopWatch : ");
+            int startTime =Convert.ToInt32 ( Console.ReadLine());
+            if (startTime == 1)
+            {
+                stopWatch.Start();
+                Console.WriteLine("Enter 1 to stop the StopWatch : ");
+                int stopTime = Convert.ToInt32(Console.ReadLine());
+                if (stopTime == 1)
+                {
+                    //Thread.Sleep(10000);
+                    stopWatch.Stop();
+
+                    // Get the elapsed time as a TimeSpan value.
+                    TimeSpan ts = stopWatch.Elapsed;
+
+                    // Format and display the TimeSpan value.
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                    if (startTime == 1 && stopTime == 1)
+                    {
+                        Console.WriteLine("Elapsed Time : " + elapsedTime);
+                    }
+                }
+                else
+                    Console.WriteLine("Try Again.");
+            }
+            else
+                Console.WriteLine("Try again.");
+            Console.ReadKey();
         }
     }
 }
