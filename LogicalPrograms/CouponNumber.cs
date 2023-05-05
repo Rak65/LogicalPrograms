1a8f18441e910c0318d54280d5f1dd4c5fa5fb58
Rak65 <rakeshp6505@gmail.com>
2023-05-05 11:53:55 +0530

    Generate Coupon Numbers.

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class CouponNumber
+    {
+        public static void Coupon()
+        {
+            Console.Write("Enter the number of coupons to generate: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] coupons = GenerateCoupons(n);
+            Console.WriteLine("The generated coupons are:");
+            foreach (string coupon in coupons)
+            {
+                Console.WriteLine(coupon);
+            }
+        }
+
+        static string[] GenerateCoupons(int n)
+        {
+            const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const int codeLength = 10;
+
+            string[] coupons = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string coupon = " ";
+                for (int j = 0; j < codeLength; j++)
+                {
+                    coupon += alphabet[new Random().Next(alphabet.Length)];
+                }
+                coupons[i] = coupon;
+            }
+
+            return coupons;
+        }
+    }
+
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,24 +8,7 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            Console.WriteLine("Enter any key to start the StopWatch : ");
-            Console.ReadKey();
-            stopWatch.Start();
-            Console.WriteLine("Enter any key to stop the StopWatch : ");
-            Console.ReadKey();  
-            //Thread.Sleep(10000);
-            stopWatch.Stop();
-
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = stopWatch.Elapsed;
-
-            // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-
-            Console.WriteLine("Elapsed Time : " + elapsedTime);
+            CouponNumber.Coupon();
             Console.ReadKey();
         }
     }
